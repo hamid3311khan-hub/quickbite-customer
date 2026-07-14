1f9a430c68fd1e6dfa9116eee891d3e959a64371
hamid3311khan-hub <hamid3311khan@gmail.com>
2026-07-14 16:15:23 +0530

    Update cart.js

--- public/cart.js
+++ public/cart.js
@@ -3,9 +3,3 @@ function goToCart(){
   if(cart.length === 0) return alert("Cart khali hai bhai");
   window.location.href = '/cart.html';
 }
-<script src="payment.js"></script>  <!-- NAYA -->
-<script>
-  let cart = JSON.parse(localStorage.getItem('cart')) || [];
-  document.getElementById('cart-items').innerHTML = cart.map(i=>`<div>${i.name} - ₹${i.price}</div>`).join('');
-  document.getElementById('total').innerText = cart.reduce((s,i)=>s+i.price,0);
-</script>
