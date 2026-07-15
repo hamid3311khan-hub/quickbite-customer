e795a4aefc594fe0b3551b21d79abcf5233d800c
hamid3311khan-hub <hamid3311khan@gmail.com>
2026-07-16 01:39:50 +0530

    Update cart.js

--- public/cart.js
+++
@@ -1,5 +0,0 @@
-function goToCart(){
-  let cart = JSON.parse(localStorage.getItem('cart')) || [];
-  if(cart.length === 0) return alert("Cart khali hai bhai");
-  window.location.href = '/cart.html';
-}
